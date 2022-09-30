536189a14cdc852be9a18264c2704c8ef111d999
Timo-Gloudemans <9018718@student.zadkine.nl>
2022-09-30 10:21:49 +0200

    changes

---
+++ DOM Counter/app.js
@@ -0,0 +1,20 @@
+let button = document.getElementById("btn");
+let button2 = document.getElementById("btn2");
+let count = 0;
+let h1 = document.getElementById("h1");
+
+function counter (){
+    count += 1;
+    h1.innerText = count + " keer geklikt";
+    document.body.style.backgroundColor = "blue";
+    button.style.backgroundColor = "red";
+}
+
+function counter2 (){
+    count -= 1;
+    h1.innerText = count + " keer geklikt";
+    document.body.style.backgroundColor = "red";
+    button2.style.backgroundColor = "blue";
+    
+}
+
---
+++ app.js
@@ -0,0 +1,4 @@
+let h1 = document.getElementById("h1")
+
+h1.style.color = "blue";
+h1.innerText = "Ik ben klaar met deze opdracht!";
