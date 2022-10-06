273d10621838fe27131d5d486a8a0e74a483acea
Timo-Gloudemans <9018718@student.zadkine.nl>
2022-10-06 12:40:35 +0200

    changes

---
+++ Week 6/Opdracht 6.4/app.js
@@ -0,0 +1,7 @@
+var calculate = (x, y) => x + y;
+var multiplyByFive = x => x * 5;
+var calculateMinutes = x => x / 60;
+
+alert(calculate(1954, 3257));
+alert(multiplyByFive(5857));
+alert(calculateMinutes(3600));
