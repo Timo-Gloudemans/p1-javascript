c51c5a5755a94df49c519b9ebb4e912924c0a825
Timo-Gloudemans <9018718@student.zadkine.nl>
2022-10-06 10:28:17 +0200

    6.2

---
+++ Week 6/Opdracht 6.2/app.js
@@ -0,0 +1,16 @@
+let h1 = document.getElementById("h1")
+
+h1.style.color = "blue";
+h1.innerText = "Ik ben klaar met deze opdracht!";
+
+
+
+
+
+
+
+function kleur_aanpassen (){
+    h1.style.color = "purple";
+    h1.innerText = "Inhoud aangepast!"
+}
+
